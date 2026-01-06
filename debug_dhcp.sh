77f4913a8fcaf2d77b9feed1f5bcf7a9591200d1
d <ddher101@gitlab.com>
2026-01-06 17:13:46 -0500

    partial

--- debug_dhcp.sh
+++ debug_dhcp.sh
@@ -5,7 +5,7 @@ echo "=== Checking if dnsmasq is running ==="
 ps aux | grep dnsmasq | grep -v grep
 
 echo -e "\n=== Checking interface configuration ==="
-ip addr show wlan0
+ip addr show wlan1
 
 echo -e "\n=== Checking IP forwarding ==="
 sysctl net.ipv4.ip_forward
@@ -17,4 +17,4 @@ echo -e "\n=== Checking if port 67 is listening ==="
 sudo netstat -ulnp | grep :67
 
 echo -e "\n=== Watching for DHCP traffic (press Ctrl+C after 10 seconds) ==="
-sudo tcpdump -i wlan0 -vv port 67 or port 68
+sudo tcpdump -i wlan1 -vv port 67 or port 68
